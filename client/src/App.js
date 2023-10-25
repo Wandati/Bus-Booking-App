53b756b267198803e33ab9fc432226ed79d2bb3b
Wandati <Petersimon801@gmail.com>
2023-10-25 10:35:23 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-center mt-4">BOOK YOUR TICKET TODAY!</h1>
+      </div>
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Footer.js
@@ -0,0 +1,20 @@
+// export default function Footer() {
+//   return (
+//     <footer className="bg-dark mt-2 text-white text-center">
+//       <p>&copy;2023.All Rights Reserved</p>
+//       <p>Email us at Nextgen@gmail.com</p>
+//       <p>Call us at 0029230132013</p>
+//     </footer>
+//   );
+// }
+export default function Footer() {
+  return (
+    <footer class="footer mt-auto py-3 bg-body-tertiary">
+      <div class="container">
+        <span class="text-body-secondary">
+          Place sticky footer content here.
+        </span>
+      </div>
+    </footer>
+  );
+}
