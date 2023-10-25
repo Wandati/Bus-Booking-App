9b12890453f23b74f46831e46813fd5b15ffdce7
NoobStevie <kamaustevek78@gmail.com>
2023-10-25 20:38:09 +0300

    Home Updated

---
+++ client/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="jumbotron">
+                <h1>Welcome to our Bus Booking Service App</h1>
+
+                <p className="lead">Easily reserve your bus tickets with no hassle!</p>
+                <p>Using this app, you can effortlessly search for bus routes, verify seat availability and securely complete online reservations.</p>
+                <p>Discover a dependable and convenient mode of travel by bus. Commence your journey with us now!</p>
+
+                <button className="btn btn-primary">Book Now</button>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
