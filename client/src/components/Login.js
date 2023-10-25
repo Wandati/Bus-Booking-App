d5aba2eea732998e00576b73c0532b87ce1c1139
NoobStevie <kamaustevek78@gmail.com>
2023-10-25 21:28:00 +0300

    Login Function Updated

---
+++ client/src/components/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        // Login logic here
+        console.log('Login form submitted');
+        console.log('Email:', email);
+        console.log('Password:', password);
+
+        // Reset Form
+        setEmail('');
+        setPassword('');
+    };
+
+    return (
+        <div className="container">
+            <h1>Login</h1>
+            <form onSubmit={handleLogin}>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email</label>
+
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Signup;
