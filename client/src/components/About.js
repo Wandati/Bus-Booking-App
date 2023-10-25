8f56c1c9cdbd378be7381e8e576b4cc80c3a6bef
NoobStevie <kamaustevek78@gmail.com>
2023-10-25 13:04:47 +0300

    About Updated

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
+        <div className="container">
             <h1>About</h1>
-            <p>Welcome to our Bus Booking Service App! We strive to provide a seamless and convenient bus booking experience for our users.</p>
+
+            <p className="lead">Welcome to our Bus Booking Service App! We strive to provide a seamless and convenient bus booking experience for our users.</p>
             
             <p>With our app, you can easily search for bus routes, check for seat availability and make secure online bookings.</p>
             
