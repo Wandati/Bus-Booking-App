61c34da53f05f7072b4dade8e14d66a10fdb4037
NoobStevie <kamaustevek78@gmail.com>
2023-10-26 21:13:29 +0300

    Changes Done

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,37 +1,6 @@
 import React from 'react';
 
-<<<<<<< HEAD
-const Contact = () => {
-    return (
-        <div className="container">
-            <h1>Contact Us</h1>
 
-            <p className="lead"> Do you have any questions or feedback? Don't hesitate to get in touch with us!</p>
-
-            <form>
-                <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="name" />
-                </div>
-
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="email" />
-                </div>
-
-                <div className="mb-3">
-                    <label htmlFor="message" className="form-label">Message</label>
-                    <textarea className="form-control" id="message" rows="4"></textarea>
-                </div>
-
-                <button type ="submit" className="btn btn-primary">Submit</button>
-            </form>
-        </div>
-    );
-};
-
-export default Contact;
-=======
 function ContactUs() {
   return (
     <div>
@@ -55,4 +24,3 @@ function ContactUs() {
 }
 
 export default ContactUs;
->>>>>>> 13044bc7cb39512ceac9f613267c74e52ee52d78
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,59 +1,5 @@
 import React, { useState } from 'react';
 
-<<<<<<< HEAD
-const Signup = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleSignup = (e) => {
-        e.preventDefault();
-
-        // Signup Logic here
-        console.log('Signup form submitted');
-        console.log('Email:', email);
-        console.log('Password:', password);
-
-        // Reset Form
-        setEmail('');
-        setPassword('');
-    };
-
-    return (
-        <div className="container">
-            <h1>Sign Up</h1>
-            <form onSubmit={handleSignup}>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email</label>
-
-                    <input
-                        type="email"
-                        className="form-control"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-
-                    <input
-                        type="password"
-                        className="form-control"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit" className="btn btn-primary">Sign Up</button>
-            </form>
-        </div>
-    );
-};
-
-export default Signup;
-=======
 function SignUpForm() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
@@ -156,4 +102,3 @@ function SignUpForm() {
 }
 
 export default SignUpForm;
->>>>>>> 13044bc7cb39512ceac9f613267c74e52ee52d78
