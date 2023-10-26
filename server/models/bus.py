2b11539e22cac7613d4e2c2d2d337d535a317acb
Wandati <Petersimon801@gmail.com>
2023-10-26 09:35:27 +0300

    models update

--- server/models/bus.py
+++ server/models/bus.py
@@ -1,5 +1,5 @@
 from .dbconfig import db
-
+from sqlalchemy.orm import validates
 class Bus(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(10),nullable=False)
@@ -10,6 +10,12 @@ class Bus(db.Model):
     # start_point = db.Column(db.String(50), nullable=False)
     # end_point = db.Column(db.String(50), nullable=False)
     routes=db.relationship('Route',backref='bus')
+    
+    @validates('number_of_seats')
+    def validate_number_of_seats(self,key,number_of_seats):
+        if not(14<=number_of_seats<=70):
+            raise ValueError("Number of seats should be between 14 and 70")
+        return number_of_seats
     
     def __repr__(self):
         return self.name
