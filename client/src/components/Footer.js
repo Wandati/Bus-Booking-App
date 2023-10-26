fab37eae73761ede404e8fd136c3057c7288e335
NoobStevie <kamaustevek78@gmail.com>
2023-10-26 21:26:41 +0300

    Footer Updated

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,56 +1,40 @@
-// export default function Footer() {
-//   return (
-//     <footer className="bg-dark mt-2 text-white text-center">
-//       <p>&copy;2023.All Rights Reserved</p>
-//       <p>Email us at Nextgen@gmail.com</p>
-//       <p>Call us at 0029230132013</p>
-//     </footer>
-//   );
-// }
 import React from 'react';
+import { FaRegEnvelope, FaPhoneAlt } from 'react-icons/fa';
+import { AiFillFacebook, AiFillInstagram, AiFillTwitterCircle } from 'react-icons/ai';
 
-function Footer() {
+const Footer = () => {
   return (
-    <div className="footer-clean">
-      <footer>
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-sm-4 col-md-3 item">
-              <h3>Services</h3>
-              <ul>
-                <li><a href="#">Web design</a></li>
-                <li><a href="#">Development</a></li>
-                <li><a href="#">Hosting</a></li>
-              </ul>
-            </div>
-            <div className="col-sm-4 col-md-3 item">
-              <h3>About</h3>
-              <ul>
-                <li><a href="#">Company</a></li>
-                <li><a href="#">Team</a></li>
-                <li><a href="#">Legacy</a></li>
-              </ul>
-            </div>
-            <div className="col-sm-4 col-md-3 item">
-              <h3>Careers</h3>
-              <ul>
-                <li><a href="#">Job openings</a></li>
-                <li><a href="#">Employee success</a></li>
-                <li><a href="#">Benefits</a></li>
-              </ul>
-            </div>
-            <div className="col-lg-3 item social">
-              <a href="#"><i className="icon ion-social-facebook"></i></a>
-              <a href="#"><i className="icon ion-social-twitter"></i></a>
-              <a href="#"><i className="icon ion-social-snapchat"></i></a>
-              <a href="#"><i className="icon ion-social-instagram"></i></a>
-              <p className="copyright">Company Name © {new Date().getFullYear()}</p>
+    <div className="container">
+      <footer className="footer">
+        <div className="row">
+          <div className="col-md-6">
+            <p>&copy; {new Date().getFullYear()} Your Bus Booking Service. All rights reserved.</p>
+            <p>
+              <FaRegEnvelope /> lawttym@gmail.com
+            </p>
+            <p>
+              <FaPhoneAlt /> +254715749444
+            </p>
+          </div>
+          <div className="col-md-6 text-end">
+            <div className="social-icons">
+              <a href="https://www.facebook.com">
+                <AiFillFacebook />
+              </a>
+
+              <a href="https://www.instagram.com">
+                <AiFillInstagram />
+              </a>
+
+              <a href="https://www.twitter.com">
+                <AiFillTwitterCircle />
+              </a>
             </div>
           </div>
         </div>
       </footer>
     </div>
   );
-}
+};
 
 export default Footer;
