0e70c9be453721a630157af83e6ce5c93f63c337
NoobStevie <kamaustevek78@gmail.com>
2023-10-26 21:17:58 +0300

    Contact Updated

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,26 +1,32 @@
 import React from 'react';
 
+const Contact = () => {
+    return (
+        <div className="container">
+            <h1>Contact Us</h1>
 
-function ContactUs() {
-  return (
-    <div>
-      <h1>Contact Us</h1>
-      <p>If you have any questions or feedback, please feel free to contact us:</p>
+            <p className="lead"> Do you have any questions or feedback? Don't hesitate to get in touch with us!</p>
 
-      <div>
-        <strong>Email:</strong> <a href="kenyanhuan@gmail.com">huan@gmailcom</a>
-      </div>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="name" />
+                </div>
 
-      <div>
-        <strong>Phone:</strong> <a href="tel:+2547897653">+2547-456-789</a>
-      </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="email" />
+                </div>
 
-      <div>
-        <strong>Address:</strong> 111 Main Street, City, Country
-      </div>
+                <div className="mb-3">
+                    <label htmlFor="message" className="form-label">Message</label>
+                    <textarea className="form-control" id="message" rows="4"></textarea>
+                </div>
 
-    </div>
-  );
-}
+                <button type ="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    );
+};
 
-export default ContactUs;
+export default Contact;
