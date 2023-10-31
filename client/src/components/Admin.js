4f640d3bdd26fd707034706823d96c633c7447ac
NoobStevie <kamaustevek78@gmail.com>
2023-10-31 14:39:17 +0300

    Admin Component Updated

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Admin = () => {
-    const users = [
-        { id: 1, name: 'Michael Townley'},
-        { id: 2, name: 'Dave Norton'},
-        { id: 3, name: 'Andrew Williams'},
+    const users = [users] = useState([
+        { id: 1, name: 'Michael Townley', email: 'townley@example.com' },
+        { id: 2, name: 'Dave Norton', email: 'dnorton@example.com' },
+        { id: 3, name: 'Andrew Williams', email: 'willandre@example.com' },
 
         // Add more users if needed
-    ];
+    ]);
+
+    const [selectedUser, setSelectedUser] = useState(null);
+
+    const handleUserClick = (userId) => {
+        const user = users.find((user) => user.id === userId);
+        setSelectedUser(user);
+    };
 
     return (
         <div>
             <h2>Admin Panel</h2>
             <ul>
                 {users.map((user) => (
-                    <li key={user.id}>{user.id}: {user.name}</li>
+                    <li key={user.id} onClick={() => handleUserClick(user.id)}>
+                        {user.name}
+                    </li>
                 ))}
             </ul>
+            {selectedUser && (
+                <div>
+                    <h3>Selected User Info:</h3>
+                    <p>ID: {selectedUser.id}</p>
+                    <p>Name: {selectedUser.name}</p>
+                    <p>Email {selectedUser.email}</p>
+                </div>
+            )}
         </div>
     );
 };
