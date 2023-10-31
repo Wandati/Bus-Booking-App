4a854a8cfdbea72f2f197f91529e2636f3916f0e
NoobStevie <kamaustevek78@gmail.com>
2023-10-31 14:41:28 +0300

    App Updated

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Admin from './components/Admin';
 import About from './components/About';
 import Contact from './components/Contact';
 import Signup from './components/Signup';
@@ -17,6 +18,7 @@ function App() {
           <h1 className="text-center mt-4"><marquee width = "50%">BOOK YOUR TICKET TODAY!</marquee></h1>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/" element={<Admin />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/signup" element={<Signup />} />
