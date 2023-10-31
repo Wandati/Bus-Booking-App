57cf54f053c6140b08a01c8de6e2788db310bc4f
NoobStevie <kamaustevek78@gmail.com>
2023-10-31 15:30:32 +0300

    Route Component Added

---
+++ client/src/components/Route.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Route = () => {
+    const routes = [
+        { id: 1, source: 'Nairobi', destination: 'Mombasa', distance: 250 },
+        { id: 2, source: 'Naivasha', destination: 'Limuru', distance: 100 },
+        { id: 3, source: 'Nakuru', destination: 'Kisumu', distance: 400 },
+
+        // Add more routes as needed
+    ];
+
+    return (
+        <div>
+            <h2>Routes</h2>
+            <ul>
+                {routes.map((route) => (
+                    <li key={route.id}>
+                        <p>Source: {route.source}</p>
+                        <p>Destination: {route.destination}</p>
+                        <p>Distance: {route.distance} km</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Route;
