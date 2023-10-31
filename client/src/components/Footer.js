90d9ae82567b632e24ff9d88f2b61f5f7dc97862
NoobStevie <kamaustevek78@gmail.com>
2023-10-31 11:21:37 +0300

    Footer Updated

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,18 +2,27 @@ import React from 'react';
 import { FaRegEnvelope, FaPhoneAlt } from 'react-icons/fa';
 import { AiFillFacebook, AiFillInstagram, AiFillTwitterCircle } from 'react-icons/ai';
 
+const App = () => {
+  return (
+    <div className="app">
+      {/* Welcome to BusTrack */}
+      <Footer />
+    </div>
+  );
+};
+
 const Footer = () => {
   return (
-    <div className="container">
-      <footer className="footer">
+    <footer className="footer">
+      <div className="container">
         <div className="row">
           <div className="col-md-6">
             <p>&copy; {new Date().getFullYear()} Your Bus Booking Service. All rights reserved.</p>
             <p>
-              <FaRegEnvelope /> lawttym@gmail.com
+              <FaRegEnvelope /> info@bustrack.com
             </p>
             <p>
-              <FaPhoneAlt /> +254715749444
+              <FaPhoneAlt /> +1 123 456 7890
             </p>
           </div>
           <div className="col-md-6 text-end">
@@ -32,9 +41,9 @@ const Footer = () => {
             </div>
           </div>
         </div>
-      </footer>
-    </div>
+      </div>
+    </footer>
   );
 };
 
-export default Footer;
+export default App;
