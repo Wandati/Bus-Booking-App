4ccd8626d6ae31a2255a9e0a0ed9077be19933e4
NoobStevie <kamaustevek78@gmail.com>
2023-11-01 10:01:07 +0300

    Routes Updated

--- client/src/components/BusRoute.js
+++ client/src/components/BusRoute.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const BusRoute = () => {
-    const routes = [
-        { id: 1, source: 'Nairobi', destination: 'Mombasa', distance: 250 },
-        { id: 2, source: 'Naivasha', destination: 'Limuru', distance: 100 },
-        { id: 3, source: 'Nakuru', destination: 'Kisumu', distance: 400 },
+    const BusRoute = () => {
+        const [routes, setRoutes] = useState([]);
 
-        // Add more routes as needed
-    ];
+        useEffect(() => {
+        fetchRoutes();
+    }, []);
 
-    return (
-        <div>
-            <h2>Routes</h2>
-            <ul>
-                {routes.map((route) => (
-                    <li key={route.id}>
-                        <p>Source: {route.source}</p>
-                        <p>Destination: {route.destination}</p>
-                        <p>Distance: {route.distance} km</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+    const fetchRoutes = async () => {
+        try {
+            const response = await fetch('');
+            const data = await response.json();
+            setRoutes(data);
+        } catch (error) {
+            console.log(error);
+        }
+  };
+
+  return (
+    <div>
+      <h2>Routes</h2>
+      <ul>
+        {routes.map((route) => (
+          <li key={route.id}>
+            <p>Source: {route.source}</p>
+            <p>Destination: {route.destination}</p>
+            <p>Distance: {route.distance} km</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default BusRoute;
