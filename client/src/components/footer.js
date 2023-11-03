e898a39b1d5b2caac0bfa91e14249ca2a9d179b7
NoobStevie <kamaustevek78@gmail.com>
2023-11-03 13:04:30 +0300

    App and Footer Updated

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -10,27 +10,29 @@ import {
 function Footer() {
   return (
     <footer className="footer-clean">
-      <div className="social-container">
-        <h3>Follow Us</h3>
-        <a href="https://www.youtube.com/c/jamesqquick" className="youtube social">
-          <FontAwesomeIcon icon={faYoutube} size="2x" />
-        </a>
-        <a href="https://www.facebook.com/learnbuildteach/" className="facebook social">
-          <FontAwesomeIcon icon={faFacebook} size="2x" />
-        </a>
-        <a href="https://www.twitter.com/jamesqquick" className="twitter social">
-          <FontAwesomeIcon icon={faTwitter} size="2x" />
-        </a>
-        <a href="https://www.instagram.com/learnbuildteach" className="instagram social">
-          <FontAwesomeIcon icon={faInstagram} size="2x" />
-        </a>
-      </div>
-      <div className="company-info">
-        <p>About: Our company is dedicated to providing high-quality services.</p>
-        <p>Location: 123 Main Street, City, Country</p>
-        <p>Telephone: +1 (123) 456-7890</p>
-        <p>Email: info@bustrack.com</p>
-      </div>
+      <h3>Follow Us:</h3>
+        <div className="social-container">
+          <p>
+          <a href="https://www.youtube.com" className="youtube social">
+            <FontAwesomeIcon icon={faYoutube} size="2x" />
+          </a>
+          <a href="https://www.facebook.com" className="facebook social">
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+          </a>
+          <a href="https://www.twitter.com" className="twitter social">
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </a>
+          <a href="https://www.instagram.com" className="instagram social">
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </a>
+          </p>
+        </div>
+        <div className="company-info">
+          <p>About: Our company is dedicated to providing high-quality services.</p>
+          <p>Location: 123 Main Street, City, Country</p>
+          <p>Telephone: +1 (123) 456-7890</p>
+          <p>Email: info@bustrack.com</p>
+        </div>
     </footer>
   );
 }
