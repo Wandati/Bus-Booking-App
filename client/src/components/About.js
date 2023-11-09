2473f559f7328e619b0c140f013e06379aaeb214
Wandati <134693858+Wandati@users.noreply.github.com>
2023-11-09 22:19:48 +0300

    Update About.js

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,7 +5,7 @@ const About = ({ setUserRole }) => {
   const token = localStorage.getItem("token");
   useEffect(() => {
     if (token) {
-      fetch("http://127.0.0.1:5500/check_user", {
+      fetch("https://bus-tracker.onrender.com/check_user", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
